dfcfb3f70c112e776d915b1b2d6aa43771da91a1
AndonMitev <mitevandon94@gmail.com>
2023-10-13 21:44:40 +0300

    more tests

--- script.ts
+++ script.ts
@@ -4,13 +4,24 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.user.deleteMany();
 
-  // const user = await prisma.user.create({
-  //   data: {
-  //     name: 'test'
-  //   }
-  // });
+  const user = await prisma.user.create({
+    data: {
+      name: 'test',
+      email: 'test@abv.bg',
+      age: 15,
+      wei: 10000000000000,
+      user: {
+        create: {
+          emailUpdates: true
+        }
+      }
+    },
+    include: {
+      user: true
+    }
+  });
 
-  // console.log('user: ', user);
+  console.log('user: ', user);
 }
 
 main()
